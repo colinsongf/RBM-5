608139becb745cd75d67d8742a9eeaf7373bc0f7
olzone <osurman@gmail.com>
2015-04-09 07:26:59 +0200

    add RBM and main files, DataLoader fix

--- DataLoader.py
+++ DataLoader.py
@@ -1,6 +1,8 @@
-import datetime
-
 __author__ = 'Aleksander Surman'
+
+from _testcapi import the_number_three
+from time import time
+
 ##################################################### USAGE #####################################################
 #                                                                                                               #
 # First Initialize Object!                                                                                       #
@@ -20,26 +22,43 @@ __author__ = 'Aleksander Surman'
 
 import numpy as np
 
+
 class DataLoader():
 
-    def makeVisibleFromRanks(self, matrix, ranksNumber):
+    def makeVisibleFromRanks(self, matrix, K, addRanksToTuple = False, generatevBiasesInitialization  = False, generateUpdateFrequency = False):
+
+        if generatevBiasesInitialization:
+            vBiasesInitialization = np.zeros((self.K, self.M), dtype=np.float32)
+            counter = 0;
+
         visibleLayer = []
         for (indexes, ranks) in matrix: # data format (tuple of artist indexes, tuple of ranks)
-            Vtmp = np.zeros((ranksNumber, len(ranks)), dtype=np.float32)
+            Vtmp = np.zeros((K, len(ranks)), dtype=np.float32)
             for index in range(len(ranks)):
                 Vtmp[ranks[index]][index] = np.float32(1.0) # not sure whether should be row[index] - 1, because ranks are from 1
-            visibleLayer.append((indexes, Vtmp))
+                if generatevBiasesInitialization:
+                    vBiasesInitialization[ranks[index]][indexes[index]] += 1
+                    counter += 1
+            if addRanksToTuple:
+                visibleLayer.append((indexes, Vtmp, ranks))
+            else:
+                visibleLayer.append((indexes, Vtmp))
+        if generatevBiasesInitialization and generateUpdateFrequency:
+            return visibleLayer, vBiasesInitialization/counter, vBiasesInitialization.astype(np.int)
+        elif generatevBiasesInitialization:
+            return visibleLayer, vBiasesInitialization/counter,
         return visibleLayer
 
     def __init__(self,
                     trainingSetFile = "Data\TrainingSet.npy",
                     validationSetFile = "Data\ValidationSet.npy",
                     testSetFile = "Data\TestSet.npy",
-                    ranksNumber = 5,
+                    K = 5,
+                    M = 17765,
                     batchSizeForOneThread = 100,
-                    threadNumber = 10,
+                    threadsNumber = 10,
                     verbose = False):
-
+        startTime = time()
         def log(x):
             if verbose:
                 print(x)
@@ -48,13 +67,14 @@ class DataLoader():
 
             log("Initializing data loader")
 
-
+            self.K = K
+            self.M = M
 
             log("\t Loading training set")
             self.trainingSet = np.load(trainingSetFile)
 
-            log("\t Making Binary form from training set")
-            self.trainingSet = self.makeVisibleFromRanks(self.trainingSet, ranksNumber)
+            log("\t Making Binary form from training set and generating visible biases initialization")
+            self.trainingSet, self.vBiasesInitialization, self.updateFrequency = self.makeVisibleFromRanks(self.trainingSet, K, generatevBiasesInitialization = True, generateUpdateFrequency= True)
 
             self.trainingSetSize = len(self.trainingSet)
             log("\t \t Done, Size: " + str(self.trainingSetSize))
@@ -65,7 +85,7 @@ class DataLoader():
             self.validationSet = np.load(validationSetFile)
 
             log("\t Making Binary form from validation set")
-            self.validationSet = self.makeVisibleFromRanks(self.validationSet, ranksNumber)
+            self.validationSet = self.makeVisibleFromRanks(self.validationSet, K, addRanksToTuple = True)
 
             self.validationSetSize = len(self.validationSet)
             log("\t \t Done, Size: " + str(self.validationSetSize))
@@ -76,7 +96,7 @@ class DataLoader():
             self.testSet = np.load(testSetFile)
 
             log("\t Making Binary form from test set")
-            self.testSet = self.makeVisibleFromRanks(self.testSet, ranksNumber)
+            self.testSet = self.makeVisibleFromRanks(self.testSet, K)
 
             self.testSetSize = len(self.testSet)
             log("\t \t Done, Size: " + str(self.testSetSize))
@@ -84,28 +104,41 @@ class DataLoader():
 
             self.trainingMiniSetNumber = -1 # -1 is for start from 0
             self.testSetCounter = -1        # -1 is for start from 0
-            self.validationSetCounter = -1  # -1 is for start from 0
 
             self.batchSizeForOneThread = batchSizeForOneThread
-            self.threadNumber = threadNumber
+            self.threadsNumber = threadsNumber
 
             log("Initialization successful")
+            endTime = time()
+            log("Took {0:0.5f} sec".format(endTime - startTime))
 
         except:
             log("Initialization failed")
             raise
 
     def StartNewTrainingMiniSet(self):
-        self.trainingMiniSetNumber += 1
-        self.InThreadsCounter = [-1 + self.trainingMiniSetNumber * (self.threadNumber * self.batchSizeForOneThread) + i * self.batchSizeForOneThread for i in range(self.threadNumber)] # -1 is for start from 0
+        if (self.trainingMiniSetNumber + 2) * (self.threadsNumber * self.batchSizeForOneThread) <= self.trainingSetSize:
+            self.trainingMiniSetNumber += 1
+            self.InThreadsCounter = [-1 + self.trainingMiniSetNumber * (self.threadsNumber * self.batchSizeForOneThread) + i * self.batchSizeForOneThread for i in range(self.threadsNumber)] # -1 is for start from 0
+        else:
+            self.trainingMiniSetNumber += 1
+            self.InThreadsCounter = [-1 + self.trainingMiniSetNumber * (self.threadsNumber * self.batchSizeForOneThread)] # -1 is for start from 0
+
+    def StartNewEpoch(self):
+        self.trainingMiniSetNumber = -1
 
     def GiveVisibleLayerForTraining(self, threadNumber):
         self.InThreadsCounter[threadNumber] += 1
         return self.trainingSet[self.InThreadsCounter[threadNumber]]
 
-    def GiveVisibleLayerForValidation(self):
-        self.validationSetCounter = (self.validationSetCounter + 1) % self.validationSetSize
-        return self.validationSet[self.validationSetCounter]
+    def StartNewValidationSet(self):
+        threadPortion = int(self.validationSetSize / self.threadsNumber)
+        self.InValidationThreadsCounter = [-1 + i * threadPortion for i in range(self.threadsNumber)] # -1 is for start from 0
+
+
+    def GiveVisibleLayerForValidation(self, threadNumber):
+        self.InValidationThreadsCounter[threadNumber] += 1
+        return self.validationSet[self.InValidationThreadsCounter[the_number_three]]
 
     def GiveVisibleLayerForTest(self):
         self.testSetCounter = (self.testSetCounter + 1) % self.testSetSize
---
+++ main.py
@@ -0,0 +1,118 @@
+__author__ = 'Aleksander Surman'
+
+import threading
+from time import time
+
+from DataLoader import DataLoader
+from RBM import RBM, loadRBM
+import numpy as np
+
+def computeRMSE(rbm = None, dataLoader = None, threadsNumber = 10, verbose = False):
+    startTime = time()
+
+    dataLoader.StartNewValidationSet()
+
+    # sharing between thread
+    errors = []
+    errorsLock = threading.Lock()
+
+    def threadJob(threadNumber):
+        for i in range(int(dataLoader.validationSetSize/threadNumber)):
+            (vVector, v, integerV) = dataLoader.GiveVisibleLayerForValidation(threadNumber)
+            predictions = rbm.prediction((vVector, v), isValidation = True) # if isValidation = False take ~ 20 times more
+            with errorsLock:
+                errors.append(predictions - integerV)
+
+    threads = []
+    for i in range(threadsNumber):
+        threads.append(threading.Thread(target=threadJob, args=(i, )))
+        threads[i].start()
+
+    for i in range(threadsNumber):
+        threads[i].join()
+
+    errors = np.hstack(errors)
+    RMSE = np.sqrt(np.power(errors, 2).sum()/len(errors))
+
+    endTime = time()
+
+    if verbose:
+        print("RMSE: {0} \nTook: {1:0.5f} sec".format(RMSE, endTime - startTime))
+    return RMSE
+
+def learnOneEpoch(rbm = None, dataLoader = None, threadsNumber = 10, batchSizeForOneThread = 100, numberOfMiniSets = 319, verbose = False):
+    startTime = time()
+    for setNumber in range(numberOfMiniSets):
+        if setNumber + 1 == numberOfMiniSets:           # last case computing with 1 thread to reduce problem with division data onto many threads
+            startTime = time()
+
+            dataLoader.StartNewTrainingMiniSet()
+
+            for i in range(dataLoader.trainingSetSize - (setNumber + 1) * (threadsNumber * batchSizeForOneThread)):
+                rbm.learn(dataLoader.GiveVisibleLayerForTraining(0))
+
+            rbm.update(verbose=verbose)
+
+            endTime = time()
+
+            if verbose:
+                print("Finish last mini set no: {0} \nTook: {1:0.5f} sec".format(setNumber, endTime - startTime))
+        else:
+            startTime = time()
+
+            dataLoader.StartNewTrainingMiniSet()
+
+            def threadJob(threadNumber):
+                for i in range(batchSizeForOneThread):
+                    rbm.learn(dataLoader.GiveVisibleLayerForTraining(threadNumber))
+
+            threads = []
+            for i in range(threadsNumber):
+                threads.append(threading.Thread(target=threadJob, args=(i, )))
+                threads[i].start()
+
+            for i in range(threadsNumber):
+                threads[i].join()
+
+            rbm.update(verbose=verbose) # updating after one mini set
+
+            endTime = time()
+
+            if verbose:
+                print("Finish mini set no: {0} \nTook: {1:0.5f} sec".format(setNumber, endTime - startTime))
+
+    endTime = time()
+    if verbose:
+        print("Epoch took: {0:0.5f} sec".format(endTime - startTime))
+
+if __name__ == "__main__":
+
+    #configuration
+    threadsNumber = 10
+    batchSizeForOneThread = 100
+    M = 17765
+    K = 5
+    F = 100
+    learningRate = 0.1
+    momentum = 0.9
+    wDecay = 0.001
+    updateFrequencyMAX = 300
+
+    numberOfEpoch = 1
+
+    dataLoader = DataLoader(K = K, M = M, batchSizeForOneThread = batchSizeForOneThread, threadsNumber = threadsNumber, verbose = True)
+
+    whereUpdateMax = np.where(dataLoader.updateFrequency > updateFrequencyMAX)
+    dataLoader.updateFrequency[whereUpdateMax] = updateFrequencyMAX
+
+    rbm = RBM(M, K, F, learningRate, momentum, wDecay, dataLoader.vBiasesInitialization, dataLoader.updateFrequency)
+    numberOfMiniSets = int(np.floor(dataLoader.trainingSetSize / (threadsNumber * batchSizeForOneThread)))
+
+    for i in range(numberOfEpoch):
+        dataLoader.StartNewEpoch()
+        learnOneEpoch(rbm, dataLoader, threadsNumber, batchSizeForOneThread, numberOfMiniSets, verbose=True)
+        with open("RBM_RMSEs.txt", "a") as RMSEsFile:
+            RMSEsFile.write("Epoch {0}, RMSE {1}\n".format(i, computeRMSE(rbm, dataLoader, threadsNumber, verbose=True)))
+
+
+
